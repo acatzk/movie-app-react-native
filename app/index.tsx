f92eb6c26fc22d83e0fd1eb8bd8b96f7948824af
Joshua Galit <joshuaimalay@gmail.com>
2025-03-20 16:00:11 +0800

    feat: add new theme colors

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View className="bg-blue-500 flex-1 justify-center items-center">
-      <Text className="text-blue-700">Welcome the homepage!!!!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-dark-100 text-5xl font-bold">AKO SI GABOR</Text>
     </View>
   )
 }
