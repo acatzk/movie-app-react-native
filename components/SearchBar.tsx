d689d3463566603df3bc9c376848f39765cee050
Joshua Galit <joshuaimalay@gmail.com>
2025-03-23 15:09:03 +0800

    feat: fetch and display movies

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Image, TextInput } from 'react-native'
+
 import { icons } from '~/constants/icons'
 
 type SearchBar = {
@@ -9,14 +10,14 @@ type SearchBar = {
 
 const SearchBar = ({ onPress, placeholder }: SearchBar): React.JSX.Element => {
   return (
-    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
-      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-1.5">
+      <Image source={icons.search} className="w-5 h-5" resizeMode="contain" tintColor="#AB8BFF" />
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
         value=""
         onChangeText={() => {}}
-        placeholderTextColor="#a8b5db"
+        placeholderTextColor="#A8B5DB"
         className="flex-1 ml-2 text-white"
       />
     </View>
